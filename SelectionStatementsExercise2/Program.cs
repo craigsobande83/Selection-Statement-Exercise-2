ef8263e939ffca026eb4d6d21467791bd3def4d6
Craig Sobande <craigsobande83@gmail.com>
2024-06-17 22:52:08 -0400

    Exercise2 Complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Physics":
+                    Console.WriteLine(" Physics is a tough subject!");
+                    break;
+                case "Biology":
+                    Console.WriteLine(" Biolgy is too intense and graphic");
+                    break;
+                case "English":
+                    Console.WriteLine(" English is an interesting subject!");
+                    break;
+
+                case "Maths":
+                    Console.WriteLine("Maths is a great subject!");
+                    break;
+                case " Chemisty":
+                    Console.WriteLine(" The best subject  ever !");
+                    break;
+
+                default:
+                    Console.WriteLine($"This is a new subject, i havent taken it before , {subject} sounds fun ");
+                    break;
+
+
+
+
+
+
+
+
+            }
         }
     }
 }
